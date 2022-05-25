de789e9503646c9ddcca54292ac9ce6bd27ad31d
Thales Alves <thalesalves758@gmail.com>
2022-05-24 23:22:25 -0300

    Importar e renderizar tela de cadastro

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Login from './Login';
+import Register from './Register';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Login />} />
+        <Route path='/cadastro' element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
