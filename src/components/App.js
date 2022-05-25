0e62edab7ac452b2d04f12483a2a2a245f97d2d3
Thales Alves <thalesalves758@gmail.com>
2022-05-24 23:45:43 -0300

    Criar rota /hoje e utilizar context para guardar usuario globalmente

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+
+import UserContext from '../contexts/UserContext';
 
 import Login from './Login';
 import Register from './Register';
+import Today from './Today';
 
 function App() {
+  const [user, setUser] = useState('');
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/cadastro' element={<Register />} />
-      </Routes>
+      <UserContext.Provider value={{ user, setUser }}>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/cadastro' element={<Register />} />
+          <Route path='/hoje' element={<Today />} />
+        </Routes>
+      </UserContext.Provider>
     </BrowserRouter>
   );
 }
