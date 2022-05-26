7b1ab40945412f1aec9cc4c64613cf93e641cd02
Thales Alves <thalesalves758@gmail.com>
2022-05-25 23:57:14 -0300

    Carregar habitos do usuario e armazenar em um estado

--- src/components/Today.js
+++ src/components/Today.js
@@ -1,10 +1,39 @@
+import axios from "axios";
+import { useEffect, useState, useContext } from "react";
+
+import UserContext from "../contexts/UserContext";
+
 import Header from "./shared/Header";
 import Menu from "./shared/Menu";
 
+import Main from "./layout/Main";
+
 function Today() {
+  const { user } = useContext(UserContext);
+
+  const [habits, setHabits] = useState([]);
+  
+  useEffect(() => {
+    const URL = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits";
+    
+    axios
+      .get(URL, {
+        headers: {
+          "Authorization": `Bearer ${user.token}`,
+        }
+      })
+      .then(({ data }) => setHabits(data))
+      .catch(err => console.log(err.response.data));
+  }, []);
+
   return (
     <>
       <Header />
+
+      <Main>
+        Hello
+      </Main>
+
       <Menu />
     </>
   );
