f467d4841dc0a771514c0ae6cde4c57ffc06deb3
Thales Alves <thalesalves758@gmail.com>
2022-05-25 23:56:28 -0300

    Utilizar z-index

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -18,6 +18,7 @@ const HeaderContainer = styled.header`
   position: fixed;
   left: 0;
   top: 0;
+  z-index: 1;
   height: 70px;
   width: 100%;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
--- src/components/shared/Menu.js
+++ src/components/shared/Menu.js
@@ -36,6 +36,7 @@ const Footer = styled.footer`
   position: fixed;
   bottom: 0;
   left: 0;
+  z-index: 1;
   width: 100%;
   height: 70px;
   background-color: var(--white);
