de537f422f9b591afd97a9553684dec279a52159
Thales Alves <thalesalves758@gmail.com>
2022-05-26 14:53:25 -0300

    Mostrar a data atual

--- src/components/Today.js
+++ src/components/Today.js
@@ -1,5 +1,7 @@
 import axios from "axios";
+import dayjs from "dayjs";
 import { useEffect, useState, useContext } from "react";
+import styled from 'styled-components';
 
 import UserContext from "../contexts/UserContext";
 
@@ -7,6 +9,9 @@ import Header from "./shared/Header";
 import Menu from "./shared/Menu";
 
 import Main from "./layout/Main";
+import MainTitle from "./layout/MainTitle";
+import Content from "./layout/Content";
+import TopContent from "./layout/TopContent";
 
 function Today() {
   const { user } = useContext(UserContext);
@@ -26,17 +31,48 @@ function Today() {
       .catch(err => console.log(err.response.data));
   }, []);
 
+  function getCurrentDate() {
+    const weekDays = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"];
+
+    return `${weekDays[dayjs().day()]}, ${dayjs().format('DD/MM')}`;
+  }
+
   return (
     <>
       <Header />
 
       <Main>
-        Hello
+        <Content>
+          <TopContent>
+            <Titles>
+              <MainTitle>
+                { getCurrentDate() }
+              </MainTitle>
+              <Subtitle>
+                Nenhum hábito concluído ainda
+              </Subtitle>
+            </Titles>
+            <div className="teste">
+              teste
+            </div>
+          </TopContent>
+        </Content>
       </Main>
 
       <Menu />
     </>
   );
 }
+
+const Titles = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Subtitle = styled.div`
+  color: ${props => props.hasCompletedHabits ? 'var(--android-green)' : 'var(--gray)'};
+  font-size: 18px;
+  line-height: 22px;
+`;
 
 export default Today;
