9c9f25d84983596e3c176b4f2de7275afb105990
Thales Alves <thalesalves758@gmail.com>
2022-05-25 23:56:51 -0300

    Criar styled component Main

---
+++ src/components/layout/Main.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Main = styled.main`
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: var(--anti-flash-white);
+  padding: 92px 18px 110px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default Main;
