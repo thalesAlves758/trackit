4931408e2c03b3f2d4f864fa9ec491f5c9642404
Thales Alves <thalesalves758@gmail.com>
2022-05-26 22:37:13 -0300

    Criar estado dos habitos de hoje e usar context

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 
 import UserContext from '../contexts/UserContext';
+import TodayHabitsContext from '../contexts/TodayHabitsContext';
 
 import Login from './Login';
 import Register from './Register';
@@ -9,15 +10,20 @@ import Today from './Today';
 
 function App() {
   const [user, setUser] = useState({});
+  const [todayHabits, setTodayHabits] = useState([]);
 
   return (
     <BrowserRouter>
       <UserContext.Provider value={{ user, setUser }}>
-        <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/cadastro' element={<Register />} />
-          <Route path='/hoje' element={<Today />} />
-        </Routes>
+        <TodayHabitsContext.Provider value={{ todayHabits, setTodayHabits }}>
+
+          <Routes>
+            <Route path='/' element={<Login />} />
+            <Route path='/cadastro' element={<Register />} />
+            <Route path='/hoje' element={<Today />} />
+          </Routes>
+          
+        </TodayHabitsContext.Provider>
       </UserContext.Provider>
     </BrowserRouter>
   );
