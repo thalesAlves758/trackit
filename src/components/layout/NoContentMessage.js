b15456181a8a5cab3ba1487d7942b342a6704c5d
Thales Alves <thalesalves758@gmail.com>
2022-05-28 14:21:45 -0300

    Criar styled component NoContentMessage

---
+++ src/components/layout/NoContentMessage.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const NoContentMessage = styled.div`
+  color: var(--granite-gray);
+  font-size: 18px;
+  line-height: 22px;
+`;
+
+export default NoContentMessage;
